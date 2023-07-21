073784e26e34e9fc176374145ce606624c733256
unknown <chavaliinfosys@gmail.com>
2023-07-21 15:46:05 +0530

    initial commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
+    bucket = "sudhakarbucket999"
     region = "us-east-1"
-    key = "eks/terraform.tfstate"
+    key = "jenkins-server-key/terraform.tfstate"
+    access_key = "AKIA37CUHR63Z3267WHT"
+    secret_key = "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"
   }
 }
